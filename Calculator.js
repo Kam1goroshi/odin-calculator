9304312ab362ad49098f621be9ac3969c235d0db
Kam1goroshi <g.pappas.1@nup.ac.cy>
2022-06-03 00:35:47 +0300

    Attempt to make Calculator an object
    commit before revert

--- Calculator.js
+++ Calculator.js
@@ -101,153 +101,157 @@ const areInputsDefined = (a, b) => {
         return undefined;
 }
 
-let acceptDot = true;
-const digitStack1 = new DigitStack();
-const digitStack2 = new DigitStack();
-const digitStack3 = new DigitStack();
-let result = "";
-let display = "";
-/**
- * Common input state machine for every position "? op ? op ?"
- * If the stack is empty, it accepts '-' and doesn't accept 0
- * If it receives del it deletes a character instead of pushing something to the stack
- * @param {',', 'del', '.', R, '-'} input calculator operation
- * @param {DigitStack} stack the input stack
- * @returns true upon using an input
- */
-const getInputHelper = (input, stack) => {
-    if (input === 'del') {
-        if (stack.isEmpty())
+export default class Calculator {
+    constructor() {
+        this.acceptDot = true;
+        this.digitStack1 = new DigitStack();
+        this.digitStack2 = new DigitStack();
+        this.digitStack3 = new DigitStack();
+        this.result = "";
+        this.display = "";
+        this.getInput = getFirstNumberInput;
+    }
+    /**
+     * Common input state machine for every position "? op ? op ?"
+     * If the stack is empty, it accepts '-' and doesn't accept 0
+     * If it receives del it deletes a character instead of pushing something to the stack
+     * @param {',', 'del', '.', R, '-'} input calculator operation
+     * @param {DigitStack} stack the input stack
+     * @returns true upon using an input
+     */
+    getInputHelper = (input, stack) => {
+        if (input === 'del') {
+            if (stack.isEmpty())
+                return true;
+            if (stack.pop() === '.') {
+                acceptDot = true;
+            }
             return true;
-        if (stack.pop() === '.') {
-            acceptDot = true;
-        }
-        return true;
-    } else if (stack.isEmpty()) {
-        if ((!isNaN(input) && input !== 0) || input === '-') {
+        } else if (stack.isEmpty()) {
+            if ((!isNaN(input) && input !== 0) || input === '-') {
+                stack.push(input);
+                return true;
+            }
+        } else if (!isNaN(input)) {
             stack.push(input);
             return true;
+        } else if (input === '.' && acceptDot === true) {
+            stack.push('.');
+            acceptDot = false;
+            return true;
         }
-    } else if (!isNaN(input)) {
-        stack.push(input);
-        return true;
-    } else if (input === '.' && acceptDot === true) {
-        stack.push('.');
-        acceptDot = false;
-        return true;
+        return false;
     }
-    return false;
-}
-/**
- * Part of the input state machine.
- * State: expecting a and op1 in "a op1 b op2 c op3". Simplified, the first number.
- * If op1 is add/sub then we move to the state without priority in order to be able to receive C too
- * If op1 is div/mult then we move to the state with priority where a div/mult b should happen
- * @param {'del', '.', '/', '*', '-', num} input calculator operation
- */
-const getFirstNumberInput = (input) => {
-    if (!getInputHelper(input, digitStack1) && (digitStack1.size > 1)) {
-        if (input === '+' || input === '-') {
-            digitStack2.push(input);
-            getInput = getSecondNumberInput;
-        } else if (input === '*' || input === '/') {
-            digitStack2.push(input);
-            getInput = getPrioritySecondNumberInput;
-        }
-    }
-}
-
-/**
- * Part of the input state machine.
- * State: expecting b and op2 in "a op1 b op2 c op3". Simplified, the second number.
- * If op2 is add/sub then "a op1 b" is calculated into a, and op1 becomes op2
- * If op2 is div/mult then we move to the state where c and op3 are expected 
- * @param {'del', '.', '/', '*', '-', num} input calculator operation
- */
-const getSecondNumberInput = (input) => {
-    if (!getInputHelper(input, digitStack2) && (digitStack2.size > 1)) {
-        if (input === '+' || input === '-') {
-            const result = "" + (digitStack1.toNumber() + digitStack2.toNumber());
-            for (let i = 0; i < result.length; i++) {
-                digitStack1.push(result.charAt(i));
-            }
-            digitStack2.push(input);
-        } else if (input === '*' || input === '/') {
-            digitStack3.push(input);
-            getInput = getThirdNumberInput;
-        }
-    } else if (digitStack2.isEmpty()) {
-        getInput = getFirstNumberInput;
-    }
-}
-
-/**
- * Part of the input state machine.
- * State: expecting b and op2 in "a op1 b op2 c op3" with priority. 
- * Simplifying state: the second number, but regardless of the next stage, calculate "a op1 b" 
- * and return to this state, or the no-priority version of it.
- * If op2 is add/sub then "a op1 b" is calculated and the machine moves to non-priority version
- * If op2 is div/mult then we move to the state where c and op3 are expected
- * a always becomes the result, op1 becomes op2
- * @param {'del', '.', '/', '*', '-', num} input calculator operation
- */
-const getPrioritySecondNumberInput = (input) => {
-    if (!getInputHelper(input, digitStack2) && (digitStack2.size > 1)) {
-        if (input === '+' || input === '-' || input === '*' || input === '/') {
-            const op = digitStack2.popReverse();
-            result = "";
-            if (op === '*') {
-                result += (digitStack1.toNumber() * digitStack2.toNumber());
-            } else if (op === '/') {
-                result += (digitStack1.toNumber() / digitStack2.toNumber());
-            }
-            for (let i = 0; i < result.length; i++) {
-                digitStack1.push(result.charAt(i));
-            }
-            digitStack2.push(input);
-            //Remove priority, now the calculator is in a+.. state
-            if (input === '+' || input === '-')
-                getInput = getSecondNumberInput;
-        }
-    } else if (digitStack2.isEmpty()) {
-        getInput = getFirstNumberInput;
-    }
-}
-
-/**
- * Part of the input state machine.
- * State: expecting c and op3 in "a op1 b op2 c op3" with priority (assignment limitation: only 1 operation at a time). 
- * I barely break the limitation when receiving "a add/sub b mul/div c add/sub" in order to keep calculating after receing operations
- * Simplifying state: the third number and operand, calculate b op2 c, onto b. 
- * If op3 is add/sub, calculate a op1 b' onto a, op1 becomes op3 and move to second input state without priority.
- * If op3 is div/mult op2 becomes op3
- * @param {'del', '.', '/', '*', '-', num} input calculator operation
- */
-const getThirdNumberInput = (input) => {
-    if (!getInputHelper(input, digitStack3) && (digitStack3.size > 1)) {
-        if (input === '*' || input === '/' || input === '+' || input === '-' ) {
-            const op = digitStack3.popReverse();
-            result = "";
-            if (op === '*') {
-                result += (digitStack2.toNumber() * digitStack3.toNumber());
-            } else if (op === '/') {
-                result += (digitStack2.toNumber() / digitStack3.toNumber());
-            }
-            for (let i = 0; i < result.length; i++) {
-                digitStack2.push(result.charAt(i));
-            }
-            if(input === '+' || input === '-'){
-                result = "" + (digitStack1.toNumber() + digitStack2.toNumber());
-                console.log("res: "+ result)
+    /**
+     * Part of the input state machine.
+     * State: expecting a and op1 in "a op1 b op2 c op3". Simplified, the first number.
+     * If op1 is add/sub then we move to the state without priority in order to be able to receive C too
+     * If op1 is div/mult then we move to the state with priority where a div/mult b should happen
+     * @param {'del', '.', '/', '*', '-', num} input calculator operation
+     */
+    getFirstNumberInput = (input) => {
+        if (!getInputHelper(input, digitStack1) && (digitStack1.size > 1)) {
+            if (input === '+' || input === '-') {
                 digitStack2.push(input);
+                getInput = getSecondNumberInput;
+            } else if (input === '*' || input === '/') {
+                digitStack2.push(input);
+                getInput = getPrioritySecondNumberInput;
+            }
+        }
+    }
+
+    /**
+     * Part of the input state machine.
+     * State: expecting b and op2 in "a op1 b op2 c op3". Simplified, the second number.
+     * If op2 is add/sub then "a op1 b" is calculated into a, and op1 becomes op2
+     * If op2 is div/mult then we move to the state where c and op3 are expected 
+     * @param {'del', '.', '/', '*', '-', num} input calculator operation
+     */
+    getSecondNumberInput = (input) => {
+        if (!getInputHelper(input, digitStack2) && (digitStack2.size > 1)) {
+            if (input === '+' || input === '-') {
+                const result = "" + (digitStack1.toNumber() + digitStack2.toNumber());
                 for (let i = 0; i < result.length; i++) {
                     digitStack1.push(result.charAt(i));
                 }
-                getInput = getSecondNumberInput;
+                digitStack2.push(input);
+            } else if (input === '*' || input === '/') {
+                digitStack3.push(input);
+                getInput = getThirdNumberInput;
             }
+        } else if (digitStack2.isEmpty()) {
+            getInput = getFirstNumberInput;
         }
-    } else if (digitStack3.isEmpty()) {
-        getInput = getSecondNumberInput;
+    }
+
+    /**
+     * Part of the input state machine.
+     * State: expecting b and op2 in "a op1 b op2 c op3" with priority. 
+     * Simplifying state: the second number, but regardless of the next stage, calculate "a op1 b" 
+     * and return to this state, or the no-priority version of it.
+     * If op2 is add/sub then "a op1 b" is calculated and the machine moves to non-priority version
+     * If op2 is div/mult then we move to the state where c and op3 are expected
+     * a always becomes the result, op1 becomes op2
+     * @param {'del', '.', '/', '*', '-', num} input calculator operation
+     */
+    getPrioritySecondNumberInput = (input) => {
+        if (!getInputHelper(input, digitStack2) && (digitStack2.size > 1)) {
+            if (input === '+' || input === '-' || input === '*' || input === '/') {
+                const op = digitStack2.popReverse();
+                result = "";
+                if (op === '*') {
+                    result += (digitStack1.toNumber() * digitStack2.toNumber());
+                } else if (op === '/') {
+                    result += (digitStack1.toNumber() / digitStack2.toNumber());
+                }
+                for (let i = 0; i < result.length; i++) {
+                    digitStack1.push(result.charAt(i));
+                }
+                digitStack2.push(input);
+                //Remove priority, now the calculator is in a+.. state
+                if (input === '+' || input === '-')
+                    getInput = getSecondNumberInput;
+            }
+        } else if (digitStack2.isEmpty()) {
+            getInput = getFirstNumberInput;
+        }
+    }
+
+    /**
+     * Part of the input state machine.
+     * State: expecting c and op3 in "a op1 b op2 c op3" with priority (assignment limitation: only 1 operation at a time). 
+     * I barely break the limitation when receiving "a add/sub b mul/div c add/sub" in order to keep calculating after receing operations
+     * Simplifying state: the third number and operand, calculate b op2 c, onto b. 
+     * If op3 is add/sub, calculate a op1 b' onto a, op1 becomes op3 and move to second input state without priority.
+     * If op3 is div/mult op2 becomes op3
+     * @param {'del', '.', '/', '*', '-', num} input calculator operation
+     */
+    getThirdNumberInput = (input) => {
+        if (!getInputHelper(input, digitStack3) && (digitStack3.size > 1)) {
+            if (input === '*' || input === '/' || input === '+' || input === '-') {
+                const op = digitStack3.popReverse();
+                result = "";
+                if (op === '*') {
+                    result += (digitStack2.toNumber() * digitStack3.toNumber());
+                } else if (op === '/') {
+                    result += (digitStack2.toNumber() / digitStack3.toNumber());
+                }
+                for (let i = 0; i < result.length; i++) {
+                    digitStack2.push(result.charAt(i));
+                }
+                if (input === '+' || input === '-') {
+                    result = "" + (digitStack1.toNumber() + digitStack2.toNumber());
+                    console.log("res: " + result)
+                    digitStack2.push(input);
+                    for (let i = 0; i < result.length; i++) {
+                        digitStack1.push(result.charAt(i));
+                    }
+                    getInput = getSecondNumberInput;
+                }
+            }
+        } else if (digitStack3.isEmpty()) {
+            getInput = getSecondNumberInput;
+        }
     }
 }
-let getInput = getFirstNumberInput;
