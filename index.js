7ab9248a947b050dafd364eb674fbb7a68891a17
Kam1goroshi <g.pappas.1@nup.ac.cy>
2022-06-03 12:09:06 +0300

    Include calculator that accepts input
    It does not yet do calculate, input tests seem okay so far.

--- index.js
+++ index.js
@@ -1,7 +1,105 @@
-getInput(input){
-    console.log(input);
+/**
+ * A queue node for the calculator. 
+ */
+class CalNode {
+    constructor(value, acceptsDot, isOperand) {
+        this.value = value;
+        this.isOperand = isOperand;
+        this.acceptsDot = acceptsDot;
+        this.next = null;
+        this.prev = null;
+    }
+
+    addNext(value, isOperand) {
+        if (value === '.' && this.acceptsDot) { //Add a dot and don't allow more dots
+            this.next = new CalNode(value, false, isOperand);
+            this.next.prev = this;
+            return true;
+        } else if (isOperand && !isNaN(this.value)) { //Add an operand and allow dots
+            this.next = new CalNode(value, true, isOperand);
+            this.next.prev = this;
+            return true;
+        } else if (!isNaN(value)) { //Add a number, and allow dot if dot was allowed here
+            this.next = new CalNode(value, this.acceptsDot, isOperand);
+            this.next.prev = this;
+            return true;
+        }
+        return false;
+    }
 }
 
+class Calculator {
+    constructor(displayElement) {
+        this.displayElement = displayElement;
+        this.head = null;
+        this.tail = null;
+    }
+
+    erase(){
+        while(this.head){
+            this.del();
+        }
+    }
+
+    del() {
+        if (this.tail === null)
+            return;
+        if (this.tail === this.head) {
+            this.displayElement.textContent = "";
+            this.tail = null;
+            this.head = null;
+            return;
+        }
+        this.tail = this.tail.prev;
+        this.tail.next.prev = null;
+        this.tail.next = null;
+        this.displayElement.textContent = this.displayElement.textContent.substring(0, this.displayElement.textContent.length - 1);
+    }
+
+    addInput(value) {
+        const isInputOperand = (value === '+' || value === '-' || value === '*' || value === '/');
+        if (this.head === null) {
+            if (isNaN(value) && (value !== '+' && value !== '-' && value !== '.'))
+                return;
+            this.head = new CalNode(value, value !== '.', isInputOperand);
+            this.tail = this.head;
+            this.displayElement.textContent += value;
+            return;
+        }
+        if (isInputOperand && this.tail.isOperand) {
+            this.tail.value = value;
+            this.displayElement.textContent = this.displayElement.textContent.substring(0, this.displayElement.textContent.length - 1)
+            this.displayElement.textContent += value;
+        }
+        if (this.tail.addNext(value, isInputOperand)) {
+            this.displayElement.textContent += value;
+            this.tail = this.tail.next;
+        }
+    }
+
+    toString() {
+        let output = "";
+        let iter = this.head;
+        while (iter) {
+            output = output.concat(iter.value)
+            iter = iter.next;
+        }
+        return output;
+    }
+}
+
+const displayElement = document.getElementById('display');
+let calculator = new Calculator(displayElement);
+
+const getInput = (input) => {
+    if (input == 'del')
+        calculator.del();
+    else if (input == 'c')
+        calculator.erase();
+    else
+        calculator.addInput(input);
+    console.log(calculator.toString());
+}
 //Bind actions, the lazy way, no need to hard code
 //If needed improve later
 let bindKeys = () => {
@@ -34,6 +132,9 @@ let bindKeys = () => {
                 break;
             case '/':
                 str = 'div';
+                break;
+            case '.':
+                str = 'dot';
                 break;
         }
         const element = document.querySelector(`.k${str}`);
